a7224df1250eca73d7807bfcb164be437eb8b336
Aeryan <raunoita@gmail.com>
2021-01-02 00:19:46 +0200

    Double the buttons

--- TaskList.py
+++ TaskList.py
@@ -1,3 +1,4 @@
+import json
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.popup import Popup
@@ -21,27 +22,40 @@ class TaskList:
 
 def create_task_popup(instance):
     popup = Popup(title="Create new task",
-                  content=Label(text='Ohh, you found me!'),
                   size_hint=(0.5, 0.5))
+    popup.add_widget(NewTaskListPopupContent(popup))
     popup.open()
+
+
+class NewTaskPopupContent(BoxLayout):
+
+    def __init__(self, popup_instance, **kwargs):
+        super(NewTaskPopupContent, self).__init__(orientation='vertical', **kwargs)
+        self.add_widget(Label(text='Ohh, you found me!'))
+        self.add_widget(Button(text="Cancel", on_press=lambda *args: popup_instance.dismiss()))
 
 
 def create_tasklist_popup(instance):
     popup = Popup(title="Create new tasklist",
-                  content=Label(text='Ohh, you found me!'),
                   size_hint=(0.5, 0.5))
+    popup.add_widget(NewTaskListPopupContent(popup))
     popup.open()
+
+
+class NewTaskListPopupContent(BoxLayout):
+
+    def __init__(self, popup_instance, **kwargs):
+        super(NewTaskListPopupContent, self).__init__(orientation='vertical', **kwargs)
+        self.add_widget(Label(text='Ohh, you found me!'))
+        self.add_widget(Button(text="Cancel", on_press=lambda *args: popup_instance.dismiss()))
 
 
 class New(BoxLayout):
 
     def __init__(self, **kwargs):
         super(New, self).__init__(**kwargs)
-        button_create_task = Button(text='New task')
-        button_create_task.bind(on_press=create_task_popup)
-        self.add_widget(button_create_task)
-        btn1 = Button(text='New task list')
-        btn1.bind(on_press=create_tasklist_popup)
+        self.add_widget(Button(text='New task', on_press=create_task_popup))
+        btn1 = Button(text='New task list', on_press=create_tasklist_popup)
         self.add_widget(btn1)
 
 
