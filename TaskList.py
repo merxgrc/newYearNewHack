be0b731b3d514a6d5bb2a5c0b0a3c4220df28d81
Aeryan <raunoita@gmail.com>
2021-01-03 14:28:48 +0200

    Working tasklist creation and task addition

--- TaskList.py
+++ TaskList.py
@@ -63,12 +63,34 @@ class Task:
 
 # TODO: Task list description
 class TaskList:
-    def __init__(self):
+    def __init__(self, name):
+        self.name = name
         self.tasks = []
+        self.complete = True
+
+    def add_task(self, task):
+        self.tasks.append(task)
+
+    def remove_task(self, task):
+        self.tasks.remove(task)
+
+    def update(self):
+        self.complete = True
+        for task in self.tasks:
+            if not task.complete:
+                self.complete = False
+                break
+
+    def get_data(self):
+        return {"name": self.name,
+                "completed": self.complete,
+                "tasks": self.tasks}
 
 
 # Store task in the data variable and call save()
-def save_task(name, deadline, desc, parent_list, popup_instance):
+def save_new_task(name, deadline, desc, parent_list, popup_instance):
+    if parent_list == "Choose a task list":
+        parent_list = default_list
     new_task = Task(name, deadline, desc, None)
     data["tasklists"][parent_list]["tasks"].append(new_task.get_data())
     data["tasklists"][parent_list]["completed"] = data["tasklists"][parent_list]["completed"] & True
@@ -79,9 +101,16 @@ def save_task(name, deadline, desc, parent_list, popup_instance):
     Clock.schedule_once(success_msg.dismiss, 0.8)
 
 
-# TODO: Store tasklist in the data variable and call save()
-def save_tasklist():
-    raise NotImplementedError
+def save_new_tasklist(name, popup_instance):
+    new_tasklist = TaskList(name)
+    tasklist_data = new_tasklist.get_data()
+    data["tasklists"][tasklist_data["name"]] = {"tasks": tasklist_data["tasks"],
+                                                "completed": tasklist_data["completed"]}
+    save()
+    popup_instance.dismiss()
+    success_msg = SuccessPopup("Tasklist created")
+    success_msg.open()
+    Clock.schedule_once(success_msg.dismiss, 0.8)
 
 
 # Success message popup with configurable title
@@ -113,8 +142,8 @@ class NewTaskPopupContent(BoxLayout):
             self.add_widget(Label(text="Task list"))
             tasklist_dropdown = DropDown()
             for key in list(taskset):
-                btn = Button(text=key, size_hint_y=None, height=20, on_release=lambda x: tasklist_dropdown.select(btn.text))
-                tasklist_dropdown.add_widget(btn)
+                tasklist_dropdown.add_widget(Button(text=key, size_hint_y=None, height=20,
+                                                    on_release=lambda x: tasklist_dropdown.select(x.text)))
             tasklist_dropdown_btn = Button(text="Choose a task list",
                                            on_release=tasklist_dropdown.open)
             self.add_widget(tasklist_dropdown_btn)
@@ -122,12 +151,12 @@ class NewTaskPopupContent(BoxLayout):
         self.add_widget(Label(text="Description"))
         desc_input = TextInput(hint_text="Short description of the task.")
         self.add_widget(desc_input)
-        self.add_widget(Button(text="Save", on_press=lambda *args: save_task(name_input.text,
-                                                                             deadline_input.text,
-                                                                             desc_input.text,
-                                                                             # TODO: CHANGE THIS once task lists are implemented
-                                                                             default_list,
-                                                                             popup_instance)))
+        self.add_widget(Button(text="Save", on_press=lambda *args: save_new_task(name_input.text,
+                                                                                 deadline_input.text,
+                                                                                 desc_input.text,
+                                                                                 # TODO: CHANGE THIS once task lists are implemented
+                                                                                 tasklist_dropdown_btn.text,
+                                                                                 popup_instance)))
         self.add_widget(Button(text="Cancel", on_press=lambda *args: popup_instance.dismiss()))
 
 
@@ -139,20 +168,13 @@ def create_tasklist_popup():
     popup.open()
 
 
-
 class NewTaskListPopupContent(BoxLayout):
     def __init__(self, popup_instance, **kwargs):
         super(NewTaskListPopupContent, self).__init__(orientation='vertical', **kwargs)
         self.add_widget(Label(text='List name'))
-        name_input = TextInput(hint_text ='List name', multiline = False)
+        name_input = TextInput(hint_text='List name', multiline=False)
         self.add_widget(name_input)
-        self.add_widget(Label(text='Task name'))
-        name_input = TextInput(hint_text="Task name", multiline=False)
-        self.add_widget(name_input)
-        self.add_widget(Label(text='Deadline'))
-        deadline_input = DatePicker()
-        self.add_widget(deadline_input)
-        self.add_widget(Button(text='Save', on_press=lambda *args: save_tasklist(name_input.text)))
+        self.add_widget(Button(text='Save', on_press=lambda *args: save_new_tasklist(name_input.text, popup_instance)))
         self.add_widget(Button(text="Cancel", on_press=lambda *args: popup_instance.dismiss()))
 
 
